42ceb2c80f2d24fed1f7e18bcfdcc0cffa3760b1
Linh Nguyen <linhn@mit.edu>
2021-03-20 15:23:45 -0500

    generate clouds in same x position

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import {ReactComponent as ReactLogo} from './images/cloud.svg';
 import { useState, useEffect } from 'react';
 import './styles.css';
-import cloud from './images/cloud.svg';
+import cloudImg from './images/cloud.svg';
 
 const Emoji = ({symbol, label}) => (
   <span
@@ -14,81 +14,98 @@ const Emoji = ({symbol, label}) => (
   </span>
 );
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 const numClouds = 10;
-const minCloudHeight = 20;
-const maxCloudHeight = 30;
+const minCloudHeight = 600;
+const maxCloudHeight = 900;
 function generateClouds() {
-  var clouds = [];
-  for (var i = 0; i < numClouds; i++) {
+  const {width, height} = getWindowDimensions();
+  const xOffset = width / 5;
+  let clouds = [];
+
+  for (let i = 0; i < numClouds; i++) {
     clouds.push({
       id: i,
       height: Math.floor(Math.random() * (maxCloudHeight - minCloudHeight + 1)) + minCloudHeight,
-      reflectedImage: Math.random() < 0.5,
+      xOffset: i % 2 == 0 ? xOffset : -1 * xOffset,
+      isReflected: Math.random() < 0.5,
+      isVisible: false,
       // TODO: emoji: ,
     });
   }
-  return clouds
+  return clouds;
 };
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [running, setRunning] = useState(1);
-  const [clouds, setClouds] = useState([]);
+  const [score, setScore] = useState(0);
+  const [cloudIndex, setCloudIndex] = useState(0);
+  const [clouds, setClouds] = useState(generateClouds());
 
+  const cloudIntervalTime = 2000;
   useEffect(() => {
-    for (let i = 1; i <= 1; i++) {
-      setTimeout(() => setClouds(arr => [...arr, {
-        height: Math.floor(Math.random() * (maxCloudHeight - minCloudHeight + 1)) + minCloudHeight,
-        reflectedImage: Math.random() < 0.5,
-      }]));
-    }
-  }, []);
+    const interval = setInterval(() => {
+      let newClouds = [...clouds];
+      newClouds[cloudIndex].isVisible = true;
+      setClouds(newClouds);
+      setCloudIndex(cloudIndex >= numClouds - 1 ? 0 : cloudIndex + 1);
+    }, cloudIntervalTime);
+    return () => clearInterval(interval);
+  }, [clouds]);
 
-  console.log(clouds);
   return (
-    
     <div className="App">
-        <p>
-          Number of clouds: {count}
-        </p>
-        <button onClick={() => {
-          setCount(0);
-          setRunning(1);
-        }}>
-          Start over
-        </button>
-
-        <p>
-          You're doing great! <Emoji symbol="😅" label="nervous-laugh"/>
-        </p>
-        <div>
-          {clouds.map((height, reflectedImage, key) => {
-              return <img 
-                src={cloud} 
-                className="cloud"
-                alt="cloud" 
-                onAnimationEnd={() => setRunning(0)}
-                running={running} 
-                // onClick={() => {
-                //   setCount(count + 1);
-                //   setRunning(0)
-                //   }
-                // }
-              />;
-            })}
-        </div>
-        {/* <img 
-          src={cloud} 
-          className="cloud"
-          alt="cloud" 
-          onAnimationEnd={() => setRunning(0)}
-          running={running} 
-          // onClick={() => {
-          //   setCount(count + 1);
-          //   setRunning(0)
-          //   }
-          // }
-        /> */}
+      <p>
+        Score: {score}
+      </p>
+      <p>
+        You're doing great! <Emoji symbol="😅" label="nervous-laugh"/>
+      </p>
+      
+      <div>
+        {clouds.map(cloud => {
+            return <img 
+              src={cloudImg} 
+              className="cloud"
+              alt="cloud"
+              data-running={cloud.isVisible} 
+              onAnimationEnd={() => {
+                cloud.isVisible = false;
+              }}
+              onClick={() => {
+                setScore(score + 1);
+                cloud.isVisible = false;
+                }
+              }
+              style={{
+                // height: cloud.height,
+                // transform: (cloud.isReflected ? "scaleX(-1)" : "scaleX(1)") + 
+                //             ` translateX(${cloud.xOffset}px)`
+              }}
+            />;
+          })}
+      </div>
     </div>
   );
 }
